2366051c717b338c58807f070a83e90c2d6379f6
MarcinRo1234 <154052364+MarcinRo1234@users.noreply.github.com>
2024-02-11 13:31:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ let btnPause = document.querySelector(".button-pause");
 let btnStop = document.querySelector(".button-stop");
 let btnCancel = document.querySelector(".button-cancel");
 let btnArchiwum = document.querySelector(".button-archiwum");
+let btnOption = document.querySelector(".timer-option .fa-question");
 let pMain = document.querySelector(".p-main");
 let pSecond = document.querySelector(".p-second");
+let archiwum = document.querySelector(".timer-archiwum")
 let archiwumList = document.querySelector(".timer-archiwum-list");
+let timerModal = document.querySelector(".timer-modal");
+let wrapper = document.querySelector(".wrapper");
+let btnClose = document.querySelector(".close");
 let timeArray = [];
 let minutes = 0;
 let seconds = 0;
@@ -37,9 +42,18 @@ const fPause = () => {
 const fStop = () => {
     
     pSecond.innerHTML = `Twój ostatni czas: ${pMain.textContent}`
+    
     if(pMain.textContent !== "0.00") {
         pSecond.style.visibility = "visible"
         timeArray.push(pMain.textContent);
+        timeArray.forEach(time => {
+            let li = document.createElement("li");
+            li.innerHTML = `Twój wynik:   <span>${time}</span>`;
+            archiwumList.appendChild(li);
+            
+        });
+        
+        timeArray = [];
     }
     clearInterval(time)
     pMain.textContent = "0.00";
@@ -50,19 +64,27 @@ const fStop = () => {
 const fCancel = () => {
     timeArray = [];
     archiwumList.textContent = "";
+    archiwum.style.display = "none";
+    pSecond.style.visibility = "hidden";
 };
 
 const fArchwiwum = () => {
-    timeArray.forEach(time => {
-        let li = document.createElement("li");
-        li.textContent = time;
-        archiwumList.appendChild(li);
+    archiwum.style.display = "flex";
 
-    })
+    
 };
+const fOption = () => {
+    timerModal.style.visibility = "visible";
+    wrapper.style.backgroundColor = "bronze";
+}
+const fClose = () => {
+    timerModal.style.visibility = "hidden";
+}
 
 btnPlay.addEventListener("click", fPlay);
 btnPause.addEventListener("click", fPause);
 btnStop.addEventListener("click", fStop);
 btnCancel.addEventListener("click", fCancel);
 btnArchiwum.addEventListener("click", fArchwiwum);
+btnOption.addEventListener("click", fOption);
+btnClose.addEventListener("click", fClose);
